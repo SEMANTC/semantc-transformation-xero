7c770191fc4ff7c06375fd3d4b0d439b9b8e4f76
fernando@abcdataz.com <fernando@abcdataz.com>
2024-10-17 01:01:19 +1300

    upudated contact_groups

--- dbt/models/transformed/xero_contacts/xero_contact__contact_groups.sql
+++ dbt/models/transformed/xero_contacts/xero_contact__contact_groups.sql
@@ -4,10 +4,10 @@
 
 SELECT DISTINCT
     JSON_VALUE(payload, '$.contact_id') AS contact_id,
-    JSON_VALUE(group, '$.contact_group_id') AS contact_group_id,
+    JSON_VALUE(contact_group, '$.contact_group_id') AS contact_group_id,
     ingestion_time
 FROM 
     {{ source('raw', 'xero_contacts') }},
-    UNNEST(JSON_QUERY_ARRAY(payload, '$.contact_groups')) AS group
+    UNNEST(JSON_QUERY_ARRAY(payload, '$.contact_groups')) AS contact_group
 WHERE 
-    JSON_VALUE(group, '$.contact_group_id') IS NOT NULL
+    JSON_VALUE(contact_group, '$.contact_group_id') IS NOT NULL
